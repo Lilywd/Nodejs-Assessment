db3045a39842077276ab393ace8c91ab339d88fd
lilywd <ndungulilianwanjiku@gmail.com>
2023-06-25 15:03:44 +0300

    code commit

---
+++ book.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const BookSchema = new mongoose.Schema({
+    title: { type: String, required: true, max_length: 255 },
+    author: { type: String, required: true, max_length: 255 },
+    genre: { type: String, required: true, max_length: 255 },
+    published_date: { type: Date },
+    description: { type: String },
+
+    // Define the _id field as an ObjectId
+    id: { type: mongoose.ObjectId }
+});
+
+module.exports = mongoose.model('Book', BookSchema);
